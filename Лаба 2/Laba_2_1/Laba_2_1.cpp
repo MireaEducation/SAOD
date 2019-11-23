c858f11b406107c8e09190ba89be8299aeeb8ceb
AdilBikeev <bikeev1999@mail.ru>
2019-11-23 16:20:48 +0300

    Laba_2 Правки меню

--- Лаба 2/Laba_2_1/Laba_2_1.cpp
+++ Лаба 2/Laba_2_1/Laba_2_1.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include "Stack.h"
+#include "LinkedStack.h"
 
 int main()
 {
@@ -9,17 +10,19 @@ int main()
 
 	short answer1 = 10, answer2 = 10;
 	Stack stack;
+	LinkedStack stackLink;
+
 	string exp = "";
 	try
 	{
-		while (answer1 != 0) {
+		while (answer1 != 3) {
 			system("cls");
 			cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
 			cout << "Вычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
 			cout << "\nМеню - выбор структуры данных\n";
 			cout << "1) Динамический массив\n";
 			cout << "2) Однонаправленный список\n";
-			cout << "0) Выход\n";
+			cout << "3) Выход\n";
 			cout << "\nВаш выбор: ";
 			while ((!(cin >> answer1)) || cin.get() != '\n') {     //делаем проверку на ввод букв   
 				cout << "ERROR\n";
@@ -31,7 +34,8 @@ int main()
 			cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
 			switch (answer1) {
 				case 1: {
-					while (answer2 != 0) {
+#pragma region 1 задание
+					while (answer2 != 3) {
 						system("cls");
 						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
 						cout << "Вычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
@@ -50,42 +54,68 @@ int main()
 						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
 						switch (answer2)
 						{
-						case 0: {
-							break;
-						}
-						case 1:
-						{
-							cin >> exp;
-							stack.init(exp);
-
-							cout << "Математическое выражение спешно распарсено в стек" << endl;
-							system("pause");
-							break;
-						}
-						case 2:
-						{
-							if (stack.top().getOperation() != ' ')
+							case 1:
 							{
-								double res = stack.getResultExpression();
-								cout << exp << " = " << res << endl;
-								stack.pop();
+								cin >> exp;
+								stack.init(exp);
+
+								cout << "Математическое выражение спешно распарсено в стек" << endl;
+								system("pause");
+								break;
 							}
-							else {
-								cout << "Для начала введите мат. выражение" << endl;
+							case 2:
+							{
+								if (stack.top().getOperation() != ' ')
+								{
+									double res = stack.getResultExpression();
+									cout << exp << " = " << res << endl;
+									stack.pop();
+								}
+								else {
+									cout << "Для начала введите мат. выражение" << endl;
+								}
+								break;
+								system("pause");
 							}
-							break;
-							system("pause");
-						}
-						default:
-						{
-							system("pause");
-						}
-						break;
+							case 3: {
+								break;
+							}
+							default:
+							{
+								system("pause");
+							}
 						}
 					}
+					answer2 = 10;
 					break;
+#pragma endregion
 				}
 				case 2: {
+#pragma region 2 Заданме
+					
+					while (answer2 != 0) {
+						system("cls");
+						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
+						cout << "Вычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
+						cout << "\nМеню - выбор дейсвтий\n";
+						cout << "1) Записать новое математическое выражение\n";
+						cout << "2) Вывести его значение\n";
+						cout << "0) Назад\n";
+						cout << "\nВаш выбор: ";
+						while ((!(cin >> answer2)) || cin.get() != '\n') {     //делаем проверку на ввод букв   
+							cout << "ERROR\n";
+							cin.clear(); // сбрасывает все биты потока, тоесть поток становится "хорошим"
+							cin.sync();//Удалим данные из буффера
+							cout << "Повторите ввод : ";
+						}
+						system("cls");
+						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
+						switch (answer2) {
+
+						}
+#pragma endregion
+					}
+					answer2 = 10;
 					break;
 				}
 			}
