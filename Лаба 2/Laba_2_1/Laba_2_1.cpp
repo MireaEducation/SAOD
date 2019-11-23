6253abcb77ce095d95bc3799ea03815f72316e2a
AdilBikeev <bikeev1999@mail.ru>
2019-11-23 15:51:01 +0300

    Laba_2 Добавил меню с выбором структуры данных для решения задачи

--- Лаба 2/Laba_2_1/Laba_2_1.cpp
+++ Лаба 2/Laba_2_1/Laba_2_1.cpp
@@ -7,21 +7,21 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	short answer2 = 10;
+	short answer1 = 10, answer2 = 10;
 	Stack stack;
 	string exp = "";
 	try
 	{
-		while (answer2 != 0) {
+		while (answer1 != 0) {
 			system("cls");
 			cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
-			cout << "Задание 1\nВычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
-			cout << "\nМеню\n";
-			cout << "1) Записать новое математическое выражение\n";
-			cout << "2) Вывести его значение\n";
+			cout << "Вычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
+			cout << "\nМеню - выбор структуры данных\n";
+			cout << "1) Динамический массив\n";
+			cout << "2) Однонаправленный список\n";
 			cout << "0) Выход\n";
 			cout << "\nВаш выбор: ";
-			while ((!(cin >> answer2)) || cin.get() != '\n') {     //делаем проверку на ввод букв   
+			while ((!(cin >> answer1)) || cin.get() != '\n') {     //делаем проверку на ввод букв   
 				cout << "ERROR\n";
 				cin.clear(); // сбрасывает все биты потока, тоесть поток становится "хорошим"
 				cin.sync();//Удалим данные из буффера
@@ -29,74 +29,67 @@ int main()
 			}
 			system("cls");
 			cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
-			switch (answer2)
-			{
-			case 0: {
-				system("pause");
-				break;
-			}
-			case 1:
-			{
-				cin >> exp;
-				stack.init(exp);
-				
-				cout << "Математическое выражение спешно распарсено в стек" << endl;
-				system("pause");
-				break;
-			}
-			case 2:
-			{
-				if (stack.top().getOperation() != ' ')
-				{
-					double res = stack.getResultExpression();
-					cout << exp << " = " << res << endl;
-					stack.pop();
+			switch (answer1) {
+				case 1: {
+					while (answer2 != 0) {
+						system("cls");
+						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
+						cout << "Вычислить значение арифметического бесскобочного выражения, введенного в виде строки.Операнды операций – это \nцелые числа.Перед вычислением значения выражения следует проверить \nего на корректность записи операций и операндов. .\n";
+						cout << "\nМеню - выбор дейсвтий\n";
+						cout << "1) Записать новое математическое выражение\n";
+						cout << "2) Вывести его значение\n";
+						cout << "0) Назад\n";
+						cout << "\nВаш выбор: ";
+						while ((!(cin >> answer2)) || cin.get() != '\n') {     //делаем проверку на ввод букв   
+							cout << "ERROR\n";
+							cin.clear(); // сбрасывает все биты потока, тоесть поток становится "хорошим"
+							cin.sync();//Удалим данные из буффера
+							cout << "Повторите ввод : ";
+						}
+						system("cls");
+						cout << "Лабораторная работа №2 Бикеева А. И. Вариант 2\n\n";
+						switch (answer2)
+						{
+						case 0: {
+							break;
+						}
+						case 1:
+						{
+							cin >> exp;
+							stack.init(exp);
+
+							cout << "Математическое выражение спешно распарсено в стек" << endl;
+							system("pause");
+							break;
+						}
+						case 2:
+						{
+							if (stack.top().getOperation() != ' ')
+							{
+								double res = stack.getResultExpression();
+								cout << exp << " = " << res << endl;
+								stack.pop();
+							}
+							else {
+								cout << "Для начала введите мат. выражение" << endl;
+							}
+							break;
+							system("pause");
+						}
+						default:
+						{
+							system("pause");
+						}
+						break;
+						}
+					}
+					break;
 				}
-				else {
-					cout << "Для начала введите мат. выражение" << endl;
+				case 2: {
+					break;
 				}
-
-				system("pause");
-				break;
-			}
-			case 3:
-			{
-
-				system("pause");
-				break;
-			}
-			case 4:
-			{
-
-				system("pause");
-				break;
-			}
-			case 5:
-			{
-
-				system("pause");
-				break;
-			}
-			case 6:
-			{
-
-				system("pause");
-				break;
-			}
-			case 7:
-			{
-
-				system("pause");
-				break;
-			}
-			default:
-			{
-				system("pause");
-			}
-			break;
 			}
 		}
-
 	}
 	catch (const std::exception & ex)
 	{
