bdf1351c44526ca30206231a3bb22d49f07a59c2
AdilBikeev <bikeev1999@mail.ru>
2019-11-28 19:06:43 +0300

    Another_Realize_Trree Добавил некотоыре комментарии по коду

--- Лаба 4/Laba_4/Laba_4/RedBlackTree.cpp
+++ Лаба 4/Laba_4/Laba_4/RedBlackTree.cpp
@@ -10,6 +10,9 @@ Node* RedBlackTree::InsertNode(char data)
 
 	current = root;
 	parent = 0;
+
+	// Перебираем узлы дерева, вплоть до листьев
+	// или пока не встретим элемент с теми эе данными
 	while (current)
 	{
 		if (data == current->data)
@@ -18,15 +21,19 @@ Node* RedBlackTree::InsertNode(char data)
 		current = data > current->data ? current->right : current->left;
 	}
 
+	// Для нового узла создаем объект вершины
 	x = new Node();
 
+	// Если дерево имеет хотяб 1 вершину
 	if (parent) {
+		// Определяем с какой стороны вставить новый узел
 		if (data == parent->data)
 			parent->left = x;
 		else
 			parent->right = x;
 	}
 	else {
+		// Иначе делаем вершину главной
 		root = x;
 	}
 
