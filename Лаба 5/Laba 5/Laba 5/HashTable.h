1d306b8901433e90eedd25c44f9c7c6c79ce6a78
AdilBikeev <bikeev1999@mail.ru>
2019-11-30 23:27:33 +0300

    Laba_5 Создал метод для добавления нового элемент в хэш-таблицу

--- Лаба 5/Laba 5/Laba 5/HashTable.h
+++ Лаба 5/Laba 5/Laba 5/HashTable.h
@@ -40,7 +40,22 @@ private:
 	/// <summary>
 	/// Массив узлов хэш-таблицы
 	/// </summary>
-	HashTableNodePair<TKey, TValue> mass;
+	HashTableNodePair<TKey, TValue> *mass;
+
+	/// <summary>
+	/// Размер хэш-таблицы
+	/// </summary>
+	int size;
+
+	/// <summary>
+	/// Хэщ-функция
+	/// </summary>
+	/// <param name="key">Ключ элемента из хэш-таблицы</param>
+	/// <returns></returns>
+	int GetIndex(TKey key)
+	{
+		return (sizeof(key) % 12 - 'a') % size;
+	}
 public:
 
 	/// <summary>
@@ -50,6 +65,23 @@ public:
 	HashTable(int size)
 	{
 		mass = new HashTableNodePair<TKey, TValue>[size];
+	}
+
+	/// <summary>
+	/// Добавляет новый элемент в хэш таблицу
+	/// </summary>
+	/// <param name="key">Ключ элемента из хэш-таблицы</param>
+	/// <param name="value">Значение соответствующее данному ключу</param>
+	void Add(TKey key, TValue value)
+	{
+		int index = this->GetIndex(key);
+
+		HashTableNodePair<TKey, TValue> node = this->mass[index];
+
+		if(! node) {
+			node = new HashTableNodePair<TKey, TValue>(key, value);
+			mass[index] = node;
+		}
 	}
 };
 
