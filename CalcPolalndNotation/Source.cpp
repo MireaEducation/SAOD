4b2b4c506be643acda4b03028170fb0f26676f0a
AdilBikeev <bikeev1999@mail.ru>
2020-01-19 12:51:03 +0300

    Algorithms Добавли возможность считать многоразрядные числа в обратной и прямой польской нотации

--- CalcPolalndNotation/Source.cpp
+++ CalcPolalndNotation/Source.cpp
@@ -8,8 +8,9 @@ using namespace std;
 /// Вычисляет мат. выражение записанное в прямой польской нотации
 /// </summary>
 /// <param name="expression">Мат. выражение записанна¤ в прямой польской нотации</param>
+/// <param name="delimiter">Символ разделитель операндов</param>
 /// <returns></returns>
-double CalculatePreFixNotation(string expression)
+double CalculatePreFixNotation(string expression, char delimiter)
 {
 	stack<double> nums;//стек для хранения операндов (чисел)
 	double length = expression.length();//длина строки
@@ -17,31 +18,46 @@ double CalculatePreFixNotation(string expression)
 	//Пробегаемся по всех символам мат. выражения
 	for (int i = length-1; i >= 0; i--)
 	{
-		//если это число
-		if (isdigit(expression[i]))
-			nums.push(expression[i] - '0');//добавляем его в стек
-		else {
-			//Если это знак операции
-			double a = nums.top(); nums.pop();//вытаскиваем первый операнд из стека
-			double b = nums.top(); nums.pop();//вытаскиваем второй операнд из стека
+		double a = 0,
+			b = 0;
+		int n = 0;//показывает разрядность числа
 
-			switch (expression[i])//узнаем какой знак операции нам дан и на основе этого проводим вычисления и добавляем результат в стек
-			{
-			case '+':
-				nums.push(a + b);
-				break;
-			case '-':
-				nums.push(a - b);
-				break;
-			case '*':
-				nums.push(a * b);
-				break;
-			case '/':
-				nums.push(a / b);
-				break;
-			default:
-				break;
-			}
+		//Формируем первый операнда
+		while (expression[i] != delimiter) {
+			a = a + (expression[i] - '0') * pow(10, n);
+			i--; n++;
+		}
+		nums.push(a);
+		i--;//перешагиваем раздлитель
+		n = 0;//зануляем разрядность для следующего числа
+
+		//Формируем второй операнда
+		while (isdigit(expression[i])) {
+			b = b + (expression[i] - '0') * pow(10, n);
+			i--; n++;
+		}
+		nums.push(b);
+
+		//Если это знак операции
+		a = nums.top(); nums.pop();//вытаскиваем первый операнд из стека
+		b = nums.top(); nums.pop();//вытаскиваем второй операнд из стека
+
+		switch (expression[i])//узнаем какой знак операции нам дан и на основе этого проводим вычисления и добавляем результат в стек
+		{
+		case '+':
+			nums.push(a + b);
+			break;
+		case '-':
+			nums.push(a - b);
+			break;
+		case '*':
+			nums.push(a * b);
+			break;
+		case '/':
+			nums.push(a / b);
+			break;
+		default:
+			break;
 		}
 
 	}
@@ -50,11 +66,12 @@ double CalculatePreFixNotation(string expression)
 }
 
 /// <summary>
-/// Вычисляет мат. выражение записанное в обратной польской нотации
+/// Вычисляет мат. выражение записанное в прямой польской нотации
 /// </summary>
-/// <param name="expression">Мат. выражение записанна¤ в обратной польской нотации</param>
+/// <param name="expression">Мат. выражение записанна¤ в прямой польской нотации</param>
+/// <param name="delimiter">Символ разделитель операндов</param>
 /// <returns></returns>
-double CalculatePostFixNotation(string expression)
+double CalculatePostFixNotation(string expression, char delimiter)
 {
 	stack<double> nums;//стек для хранения операндов (чисел)
 	double length = expression.length();//длина строки
@@ -62,13 +79,27 @@ double CalculatePostFixNotation(string expression)
 	//Пробегаемся по всех символам мат. выражения
 	for (double i = 0; i < length; i++)
 	{
-		//если это число
-		if(isdigit(expression[i]))
-			nums.push(expression[i] - '0');//добавляем его в стек
-		else {
+		double a = 0,
+			   b = 0;
+
+			//Формируем первый операнда
+			while (expression[i] != delimiter) {
+				a = a * 10 + (expression[i] - '0');
+				i++;
+			}
+			nums.push(a);
+			i++;//перешагиваем раздлитель
+
+			//Формируем второй операнда
+			while (isdigit(expression[i])) {
+				b = b * 10 + (expression[i] - '0');
+				i++;
+			}
+			nums.push(b);
+
 			//Если это знак операции
-			double a = nums.top(); nums.pop();//вытаскиваем первый операнд из стека
-			double b = nums.top(); nums.pop();//вытаскиваем второй операнд из стека
+			a = nums.top(); nums.pop();//вытаскиваем первый операнд из стека
+			b = nums.top(); nums.pop();//вытаскиваем второй операнд из стека
 
 			switch (expression[i])//узнаем какой знак операции нам дан и на основе этого проводим вычисления и добавляем результат в стек
 			{
@@ -87,8 +118,6 @@ double CalculatePostFixNotation(string expression)
 				default:
 					break;
 			}
-		}
-
 	}
 //последний элеметн в стеке и будет окончательным резултатом вычисленного мат. выражения
 	return nums.top();
@@ -101,14 +130,15 @@ int main()
 
 	
 	string exp;
-	/*
+	
 	cout << "Введите мат. выражение в обратной польской нотации: ";
 	cin >> exp;
-	cout<<"Результат: "<<CalculatePostFixNotation(exp)<<endl;
-	*/
+	cout<<"Результат: "<<CalculatePostFixNotation(exp, '&')<<endl;	
+
+	
 	cout << "Введите мат. выражение в прямой польской нотации: ";
 	cin >> exp;
-	cout << "Результат: " << CalculatePreFixNotation(exp) << endl;
-
+	cout << "Результат: " << CalculatePreFixNotation(exp, '&') << endl;
+	
 	return 0;
 }
